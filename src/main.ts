12aa27e3678bccf0bc271b3503e6910fdd2beab8
chertik77 <denysbabych.dev@gmail.com>
2024-09-03 16:14:08 +0100

    feat: add prisma

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
+import type { NestExpressApplication } from '@nestjs/platform-express'
+
 import { NestFactory } from '@nestjs/core'
+
+import * as morgan from 'morgan'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+  app.setGlobalPrefix('api')
+
+  app.use(morgan('dev'))
+
+  app.disable('x-powered-by')
+
+  app.enableCors({ origin: process.env.ALLOWED_ORIGINS })
+
+  await app.listen(process.env.PORT!)
 }
+
 bootstrap()
