ef4e970b880f97267721492d257e7f0dbdb95933
chertik77 <denysbabych.dev@gmail.com>
2025-07-30 19:10:33 +0100

    feat: add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
-@Module({ imports: [], controllers: [], providers: [] })
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [],
+  providers: []
+})
 export class AppModule {}
